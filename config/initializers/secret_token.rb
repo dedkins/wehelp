2c2663e305366f252cdd25507f1c8b930739cbc0
David Edkins <dedkins@edkinsgroup.com>
2014-01-14 12:29:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wehelp::Application.config.secret_key_base = '9d1060f505468120edefd4a477d9cfa6101014a45fab10e891494647730435d8748d81a43f9b10b400708041f7a53f4b25be001868a51229c6019a1abcc9d8b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wehelp::Application.config.session_store :cookie_store, key: '_wehelp_session'
